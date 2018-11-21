6790c8c38da9633d5986ad8eedef8987c2b65d6c
Matthew R. Miller <mattrussmill@gmail.com>
2018-11-20 23:51:26 -0500

    Update signalsuppressor.h

--- src/signalsuppressor.h
+++ src/signalsuppressor.h
@@ -2,21 +2,16 @@
 #define SIGNALSUPPRESSOR_H
 
 #include <QObject>
-#include <QStack>
 #include <QVariant>
-class QPoint;
-class QMutex;
+#include <QMutex>
 
 class SignalSuppressor : public QObject
 {
     Q_OBJECT
 public:
     explicit SignalSuppressor(QObject *parent = nullptr);
-    ~SignalSuppressor();
     void performOperation(); // pass fn pointer?
     QVariant retrieveNewData(QVariant oldData);
-    uint16_t getBufferSize();
-    void setBufferSize(uint16_t size);
 
 signals:
     void suppressedSignal(SignalSuppressor *ptr);
@@ -25,9 +20,7 @@ public slots:
     void receiveNewData(QVariant newData);
 
 private:
-    void deleteOldData(); //must leave one item
-    QStack<QVariant> data_m; //consider ringbuffer 4 performance?
-    uint16_t maxBuffer_m = 255;
+    QVariant data_m; //consider ringbuffer 4 performance?
     QMutex mutex; //this NEEDS implemented internally as 2 threads will be calling the same file
 };
 
